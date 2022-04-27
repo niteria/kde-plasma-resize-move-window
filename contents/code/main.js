41e754358ea9c71e3543f39c68808ea377279647
Jonathan C. Pim <joncpim@gmail.com>
2022-04-27 08:52:40 +0300

    Generalized the move and resize current window function.

--- contents/code/main.js
+++ contents/code/main.js
@@ -4,40 +4,30 @@
 // checkout KWin scipting tutorial for details:
 // https://techbase.kde.org/Development/Tutorials/KWin/Scripting
 
-function growCurrentWindow() {
-  var win = workspace.activeClient;
-  win.geometry = {
-      x: win.x - 20,
-      y: win.y,
-      width: win.width + 40,
-      height: win.height,
-  }
+// width_per in [0,1] : fraction of current window's width
+// height_per in [0,1] : fraction of current window's height
+// keep_x_pos in {true,false} : move the current window in x direction?
+// keep_y_pos in {true,false} : move the current window in y direction?
+// x_per,y_per in [0,1] : 0 is left/top edge and 1 is right/bottom edge
+function moveAndResizeCurrentWindow(width_per, height_per, keep_x_pos = true, keep_y_pos = true, x_per = 0, y_per = 0) {
+	var win = workspace.activeClient;
+    var maxArea = workspace.clientArea(KWin.MaximizeArea, win);
+	var height = height_per * maxArea.height;
+	var width = width_per * maxArea.width;
+    var x_pos = keep_x_pos ? win.x : maxArea.x + x_per * (maxArea.width - width);
+    var y_pos = keep_y_pos ? win.y : maxArea.y + y_per * (maxArea.height - height);
+
+	win.geometry = {
+		x: x_pos,  
+		y: y_pos,
+		width: width,
+		height: height, 
+	}
 }
 
-function shrinkCurrentWindow() {
-  var win = workspace.activeClient;
-  win.geometry = {
-      x: win.x + 20,
-      y: win.y,
-      width: win.width - 40,
-      height: win.height,
-  }
-}
-
-function setCurrentWindowSize() {
-  var win = workspace.activeClient;
-  var height = workspace.displayHeight;
-  var half = workspace.displayWidth / 2;
-  var marginFromLeft = half / 2;
-
-  win.geometry = {
-      x: marginFromLeft,
-      y: win.y,
-      width: half,
-      height: win.height,
-  }
-}
-
-registerShortcut("Make current window size 1/2 of screen", "Set the current window size to 1/2 of screen and center it", "Meta+M", setCurrentWindowSize);
-registerShortcut("Grow current window horizontally", "Make current window size 40 px wider", "Meta+.", growCurrentWindow);
-registerShortcut("Shrink current window horizontally", "Make current window 40 px narrower", "Meta+,", shrinkCurrentWindow);
+registerShortcut("Make current window 1/3 width, full height, and left", "Set current window to 1/3 width, full height and left", "Meta+D", function() {moveAndResizeCurrentWindow(1/3, 1, false, false, 0, 0)});
+registerShortcut("Make current window 2/3 width, full height and left", "Set current window to 2/3 width, full height and left", "Meta+Shift+D", function() {moveAndResizeCurrentWindow(2/3, 1, false, false, 0, 0)});
+registerShortcut("Make current window 1/3 width, full height, and right", "Set current window to 1/3 width, full height and right", "Meta+G", function() {moveAndResizeCurrentWindow(1/3, 1, false, false, 1, 0)});
+registerShortcut("Make current window 2/3 width, full height and right", "Set current window to 2/3 width, full height and right", "Meta+Shift+G", function() {moveAndResizeCurrentWindow(2/3, 1, false, false, 1, 0)});
+registerShortcut("Make current window 1/3 width, full height, and centre", "Set current window to 1/3 width, full height and centre", "Meta+F", function() {moveAndResizeCurrentWindow(1/3, 1, false, false, 0.5, 0)});
+registerShortcut("Make current window 1/4 width, full height, and centre", "Set current window to 1/4 width, full height and centre", "Meta+Shift+F", function() {moveAndResizeCurrentWindow(1/4, 1, false, false, 0.5, 0)});
